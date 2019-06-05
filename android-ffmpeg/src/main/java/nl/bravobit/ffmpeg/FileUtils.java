08f1caadc0a5f794e0508cd431db6483a4b9bb52
Liam Cottle <liam@liamcottle.com>
2019-06-05 16:23:51 +1200

    make FileUtils.getFFmpeg and FileUtils.getFFprobe public
    Reference: https://github.com/bravobit/FFmpeg-Android/issues/114#issuecomment-498932194

--- android-ffmpeg/src/main/java/nl/bravobit/ffmpeg/FileUtils.java
+++ android-ffmpeg/src/main/java/nl/bravobit/ffmpeg/FileUtils.java
@@ -13,12 +13,12 @@ class FileUtils {
     private static final String FFMPEG_FILE_NAME = "ffmpeg";
     private static final String FFPROBE_FILE_NAME = "ffprobe";
 
-    static File getFFmpeg(Context context) {
+    public static File getFFmpeg(Context context) {
         File folder = context.getFilesDir();
         return new File(folder, FFMPEG_FILE_NAME);
     }
 
-    static File getFFprobe(Context context) {
+    public static File getFFprobe(Context context) {
         File folder = context.getFilesDir();
         return new File(folder, FFPROBE_FILE_NAME);
     }
